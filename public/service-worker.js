78b065e06725c186d37c245fb510a63f99291e3b
Carl Bush <CarlBushs13@gmail.com>
2022-06-05 11:00:53 -0700

    service-worker.js activiate, fetch, install complete

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,13 +18,36 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", function (e) {
-
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.andAll(FILES_TO_CACHE);
+        })
+    );
 });
 
 self.addEventListener("activate", function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
 
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            )
+        })
+    );
 });
 
 self.addEventListener("fetch", function (e) {
-
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            return request || fetch(e.request)
+        })
+    );
 });
